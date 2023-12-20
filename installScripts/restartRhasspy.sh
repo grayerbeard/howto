10bd172ef68fa65abff725c4c3357f52fb60debf
David <david@localhost.localdomain>
2023-12-20 09:16:03 +0000

    adjusted rhasspy docker command in restart script

--- installScripts/restartRhasspy.sh
+++ installScripts/restartRhasspy.sh
@@ -1,12 +1,14 @@
 docker stop rhasspy
 docker rm rhasspy
 docker run -d -p 12101:12101 \
-      --name rhasspy \
-      --restart unless-stopped \
-      -v "$HOME/.config/rhasspy/profiles:/profiles" \
-      -v "$HOME/testAudio:/testAudio" \
-      -v "/etc/localtime:/etc/localtime:ro" \
-      --device /dev/snd:/dev/snd \
-      rhasspy/rhasspy \
-      --user-profiles /profiles \
-      --profile en
+    --name rhasspy \
+    --restart unless-stopped \
+    -v "$HOME/.config/rhasspy/profiles:/profiles" \
+    -v "/etc/localtime:/etc/localtime:ro" \
+    -v "$HOME/testAudio:/testAudio" \-v /var/run/pulse:/var/run/pulse \
+    -v "$HOME/asound.conf:/etc/asound.conf" \
+    -v /var/run/pulse:/var/run/pulse \
+    --device /dev/snd:/dev/snd \
+    rhasspy/rhasspy \
+    --user-profiles /profiles \
+    --profile en
