b34d7de7d2c9190651b22a54f0760c4fe3e71776
David <david@localhost.localdomain>
2023-12-20 09:20:17 +0000

    removed additioons to rhasspy restart that did not work

--- installScripts/restartRhasspy.sh
+++ installScripts/restartRhasspy.sh
@@ -5,9 +5,7 @@ docker run -d -p 12101:12101 \
     --restart unless-stopped \
     -v "$HOME/.config/rhasspy/profiles:/profiles" \
     -v "/etc/localtime:/etc/localtime:ro" \
-    -v "$HOME/testAudio:/testAudio" \-v /var/run/pulse:/var/run/pulse \
-    -v "$HOME/asound.conf:/etc/asound.conf" \
-    -v /var/run/pulse:/var/run/pulse \
+    -v "$HOME/testAudio:/testAudio" \
     --device /dev/snd:/dev/snd \
     rhasspy/rhasspy \
     --user-profiles /profiles \
